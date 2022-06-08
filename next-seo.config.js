dbd02e0b4e984ac0e1d1947da7d8347a9723298b
dkotsev <dobromir.kotsev@gmail.com>
2022-06-08 23:59:53 +0300

    SEO

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,15 +1,26 @@
 export default {
-    title:"NFT Gigs",
-    description: " Find NFT Jobs and Gigs",
-    openGraph: {
-      type: 'website',
-      locale: 'en_IE',
-      url: 'https://nftgigs.xyz',
-      site_name: 'SiteName',
-    },
-    twitter: {
-      handle: '@NFTGigs',
-      site: '@site',
-      cardType: 'summary_large_image',
-    },
+  title:"NFT Gigs",
+  description:"Discover NFT Jobs and NFT Gigs.",
+  canonical:"https://nftgigs.xyz",
+  openGraph:{
+    url: 'https://nftgigs.xyz',
+    title: 'Discover NFT Jobs and Gigs',
+    description: 'Discover NFT Jobs and GIGs',
+    images: [
+      {
+        url: 'https://i.ibb.co/kmxLbTm/nft-Gigs-Social-Image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Og Image Alt',
+        type: 'image/png',
+      }
+    
+    ],
+    site_name: 'NFT Gigs',
+  },
+  twitter:{
+    handle: '@NFTGigs',
+    site: 'nftgigs.xyz',
+    cardType: 'summary_large_image',
+  }
   };
